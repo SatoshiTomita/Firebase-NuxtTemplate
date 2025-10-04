2724f5726f72795cde444a22549957c6e01d92eb
sato <yamatomi3104@gmail.com>
2025-10-04 18:01:40 +0900

    vercel設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/common.scss"],
+  nitro: {
+    preset: "vercel",
+  },
 });
